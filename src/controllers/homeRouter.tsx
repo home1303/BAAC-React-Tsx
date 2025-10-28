a89afe30d2101ddd50249654027bf59a165ec148
home1303 <s6404062610031@email.kmutnb.ac.th>
2025-10-28 17:21:04 +0700

    SearchPage Process1
    arrange search container

--- src/controllers/homeRouter.tsx
+++ src/controllers/homeRouter.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../views/HomePage';
+import SearchForm from '../views/SearchForm';
 
 const HomeRouter: React.FC = () => (
 
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/home" element={<HomePage />} />
+    <Route path='/SearchForm' element={<SearchForm />} />
   </Routes>
 );
 
